79350b154973e7816966148e91c06ff6b1502095
Kai S <k.sommer@tutamail.com>
2026-05-24 15:03:19 -0600

    hyprland.nix touchup

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -69,14 +69,6 @@ in
 
 			decoration = {
 				rounding = 15;
-#				active_opacity = 1.0;
-#				inactive_opacity = 0.9;
-#				blur = {
-#					enabled = true;
-#					size = 5;
-#					passes = 3;
-#					brightness = 0.5;
-#				};
 			};
 			
 			input = {
