464ab7ace26acf80de5a22818d3a57e8f8a463fc
Kai S <k.sommer@tutamail.com>
2026-06-11 23:05:19 -0600

    implement and fixes

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -1,4 +1,4 @@
-{ config, inputs, user, ... }:
+{ config, pkgs, inputs, lib, user, hostName, ... }:
 let
 	dotfiles = "/home/${user}/nixos-config/dotfiles";
 in
@@ -8,17 +8,19 @@ in
 		inputs.stylix.homeModules.stylix
 		inputs.zen-browser.homeModules.beta
 		./programs.nix
-    	./hyprland.nix
+#    	./hyprland.nix
 	   	./stylix.nix
-    	./zen.nix
+    	./zen.nix		
 	];
 
 	programs.caelestia = {
 		enable = true;
 		cli.enable = false;
-	};
-		home.file.".config/caelestia".source =
-			config.lib.file.mkOutOfStoreSymlink "${dotfiles}/caelestia";
+	};		
+	home.file.".config/caelestia".source = 
+		config.lib.file.mkOutOfStoreSymlink "${dotfiles}/caelestia";
+	home.file.".config/hypr".source =
+		config.lib.file.mkOutOfStoreSymlink "${dotfiles}/hypr";	
 
 	home.username = "sommer";
 	home.homeDirectory = "/home/sommer";
